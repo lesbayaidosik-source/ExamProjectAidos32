2e8d43b2b5aec08a685cb2585d8c0131516c2241
lesba <Lesbayaidosik@gmail.com>
2026-04-29 14:16:26 +0500

    ready

--- class AR1_9/class AR1_9.py
+++ class AR1_9/class AR1_9.py
@@ -1,19 +1,8 @@
 import numpy as np
-
-class AR19:
-    def __init__(self, T=100, seed=42):
+class AR1_9:
+    def __init__(self,T):
         self.T = T
-        self.seed = seed
-    def generate(self, a):
-        # тұрақты нәтиже
-        np.random.seed(self.seed)
-        eps = np.random.normal(0, 1, self.T)
-        x = np.zeros(self.T)
-        for t in range(1, self.T):
-            x[t] = a * x[t-1] + eps[t]
-        # нәтижені шығарады
-        print(x)
-        return x
-# RUN
-gen = AR19(T=100)
-gen.generate(0.5)
+    def generate(self):
+        eps = np.random.normal(2 ,1, self.T)
+        return eps
+print(AR1_9(80).generate())
