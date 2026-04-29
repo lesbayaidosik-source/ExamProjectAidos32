17817e8b204045510da5a9e0e8a84cff74f9f1ff
lesba <Lesbayaidosik@gmail.com>
2026-04-29 14:17:39 +0500

    ready

--- class AR1_11/class AR1_11.py
+++ class AR1_11/class AR1_11.py
@@ -1,12 +1,25 @@
+import numpy as np
 import pandas as pd
-
 class AR1_11:
-    def __init__(self, T=100):
+    def __init__(self, T):
         self.T = T
-    def build(self):
-        df = pd.DataFrame(index=range(self.T))  # 📌 0..T-1
-        print(df.head())
+
+    def generate_ar1(self, a):
+        eps = np.random.normal(0, 1, self.T)
+        x = np.zeros(self.T)
+
+        for t in range(1, self.T):
+            x[t] = a * x[t-1] + eps[t]
+        return x
+
+    def build_dataframe(self):
+        x1 = self.generate_ar1(0.5)
+        x2 = self.generate_ar1(0.9)
+        df = pd.DataFrame({
+            'x_a_05': x1,
+            'x_a_09': x2
+        }, index=range(self.T))
+        print(df)
         return df
-# RUN
-model = AR1_11(100)
-df = model.build()
+task = AR1_11(T=100)
+df = task.build_dataframe()
